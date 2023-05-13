bf638c8ea2bc5073401321ab4d2e9b8c089d56e5
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-05-13 16:40:57 -0400

    Fix

--- learning1/target/debug/deps/learning1-611868c87562b65b.d
+++
@@ -1,5 +0,0 @@
-/Users/apa/code_challenge/rust_learning/learning1/target/debug/deps/learning1-611868c87562b65b.rmeta: src/main.rs
-
-/Users/apa/code_challenge/rust_learning/learning1/target/debug/deps/learning1-611868c87562b65b.d: src/main.rs
-
-src/main.rs:
