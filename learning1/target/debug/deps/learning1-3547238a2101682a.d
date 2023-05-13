c873c7160b0130467ca64c8d273f249ca6d9ffb8
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-05-13 16:11:40 -0400

    remove files

--- learning1/target/debug/deps/learning1-3547238a2101682a.d
+++
@@ -1,5 +0,0 @@
-/Users/apa/code_challenge/rust_learning/learning1/target/debug/deps/learning1-3547238a2101682a: src/main.rs
-
-/Users/apa/code_challenge/rust_learning/learning1/target/debug/deps/learning1-3547238a2101682a.d: src/main.rs
-
-src/main.rs:
--- learning1/target/debug/learning1.d
+++
@@ -1,1 +0,0 @@
-/Users/apa/code_challenge/rust_learning/learning1/target/debug/learning1: /Users/apa/code_challenge/rust_learning/learning1/src/main.rs
