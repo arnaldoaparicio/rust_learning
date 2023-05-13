00d22fb7e63b83beecd6af278843cf9da043efe8
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-05-13 17:02:33 -0400

    Add variables and constants

--- learning1/src/main.rs
+++ learning1/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    // println!("Hello, world!");
+    // let x = 4; //implicit type assignment
+
+    // let mut x = 4;
+    // println!("x is: {}", x);
+    // x = 5;
+    // println!("x is: {}", x);
+
+    const SECONDS_IN_MINUTES: u32 = 60;
+
+    println!("{}", SECONDS_IN_MINUTES);
+
+
 }
